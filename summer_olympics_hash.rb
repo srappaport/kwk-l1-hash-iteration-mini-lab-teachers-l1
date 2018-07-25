10d470f184d38ad741ffc577a7ef108fedbc89d4
srappaport <sarah.rappaport@greendot.org>
2018-07-25 15:38:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -6,7 +6,7 @@ end
 
 def add_a_key_value_pair
   olympics_hash=create_olympics_hash
-  olympics_hash[:Atlana] = "1996"
+  olympics_hash[:Atlanta] = "1996"
   return olympics_hash
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
