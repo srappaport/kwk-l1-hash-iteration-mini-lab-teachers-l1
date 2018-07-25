8331ebfd46605f5d070f4fa240aa2bdac0cbfd41
srappaport <sarah.rappaport@greendot.org>
2018-07-25 15:37:15 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -9,6 +9,7 @@ def add_a_key_value_pair
   puts olympics_hash
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
+add_a_key_value_pair
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
