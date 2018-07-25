2d8edd3199981c7d86b7be6cc76e2a9cc58f1bf8
srappaport <sarah.rappaport@greendot.org>
2018-07-25 15:43:18 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -21,7 +21,7 @@ def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
 end
-
+iterate_through_hash
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
